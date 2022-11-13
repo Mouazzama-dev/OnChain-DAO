3d63eee20aea9555bc32894ebbaed5b95f85fffd
Mouazzama <mouazzamabinteumer.ep19102044@gmail.com>
2022-11-13 04:20:49 -0800

    files

---
+++ hardhat/constants.js
@@ -0,0 +1,5 @@
+// Replace the value with your NFT contract address
+const CRYPTODEVS_NFT_CONTRACT_ADDRESS =
+  "0xFFb6C7C8e2578c6D32170BBb45Bf9a4Ef1AD443a";
+
+module.exports = { CRYPTODEVS_NFT_CONTRACT_ADDRESS };
